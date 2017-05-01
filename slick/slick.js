f8365cbed403de5436187ca36545ae7f173ce992
Machiko Murakami <machiko@applydigital.co>
2017-05-01 12:23:32 -0700

    expecting wordpress jQuery

--- slick/slick.js
+++ slick/slick.js
@@ -17,14 +17,15 @@
 /* global window, document, define, jQuery, setInterval, clearInterval */
 ;(function(factory) {
     'use strict';
-    if (typeof define === 'function' && define.amd) {
+    if(typeof jQuery !== 'undefined') {
+        factory(jQuery);
+    } else if (typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
     } else if (typeof exports !== 'undefined') {
         module.exports = factory(require('jquery'));
     } else {
         factory(jQuery);
     }
-    //test
 
 }(function($) {
     'use strict';
